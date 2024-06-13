a97e90b7342234dc943d744c448ff5c7e82ce4ff
No_Thing <147016635+Identity-UnkWn@users.noreply.github.com>
2024-06-13 21:06:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin:"https://conteno-front.pages.dev/",
+    origin:"https://conteno-front.pages.dev",
     credentials:true
 }))
 
