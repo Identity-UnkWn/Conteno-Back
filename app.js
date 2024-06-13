215676222179feea76c5235baaf09b9b4388feb5
No_Thing <147016635+Identity-UnkWn@users.noreply.github.com>
2024-06-13 21:18:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,10 +9,29 @@ const app = express();
 
 app.use(express.json());
 
+//app.use(cors({
+   // origin:"*",
+   // credentials:true
+//}))
+
+app.use((req, res, next) => {
+    console.log('Received request from origin:', req.headers.origin);
+    next();
+});
+
 app.use(cors({
-    origin:"*",
-    credentials:true
-}))
+    origin: 'https://conteno-front.pages.dev',
+    credentials: true,
+    optionsSuccessStatus: 200 // For legacy browser support
+}));
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://conteno-front.pages.dev');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 
 app.use("/",router);
 
